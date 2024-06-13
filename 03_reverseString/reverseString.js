fb4434dce81565a51a37610eb3e83acb2583fed7
MismoMate <165439467+MismoMate@users.noreply.github.com>
2024-06-13 03:52:04 -0500

    edit: reverse a string, a bit with extra vars

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(phrase) {
+    let tempPhrase = "";
+    tempPhrase = phrase.split("");
+    phase = tempPhrase.reverse();
+    let reversePhase = phase.join("");
+    return reversePhase;
 };
 
 // Do not edit below this line
