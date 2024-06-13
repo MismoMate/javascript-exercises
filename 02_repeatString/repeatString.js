692c32657db4645dd0cbed7e3a659645050da365
MismoMate <165439467+MismoMate@users.noreply.github.com>
2024-06-13 03:21:51 -0500

    Edit: add a for loop that concats a word together

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,10 @@
-const repeatString = function() {
-
-};
+const repeatString = function(word, num) {
+    let wordChain = "";
+    for (let i = 0; i < num; i++){
+       wordChain += word;
+    }
+    return wordChain;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
