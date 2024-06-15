e855a37dddebdcb37e51ebd0fd8519a976cea061
MismoMate <165439467+MismoMate@users.noreply.github.com>
2024-06-15 18:01:34 -0500

    edit removed elements from an array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(numList, ...excludeNums) {
+    let newNumbList = numList;
+    for (let i = 0; i < excludeNums.length; i++){
+        newNumbList = newNumbList.filter(a => a !== excludeNums[i]);  
+    }  
+    return newNumbList;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
